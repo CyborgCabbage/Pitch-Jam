b6e4d76afec5a3b015e4990544036c2403c2f108
Starbit04 <2200752@uad.ac.uk>
2023-04-01 16:19:40 +0100

    thing

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -8,17 +8,20 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] PlayerMovement playerMov;
     [SerializeField] Rigidbody2D rigbod;
 
+    Vector2 playerInput;
     string aniState = "Idle";
+    float referenceScale;
+    [SerializeField]  bool hasSetBallDir = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        referenceScale = transform.localScale.x;
     }
 
-    void setAnimation(string stateName)
+    void setAnimation(string stateName, bool force)
     {
-        if (stateName != aniState)
+        if (stateName != aniState || force)
         {
             ani.Play(stateName, 0, 0);
 
@@ -29,16 +32,54 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rigbod.velocity.magnitude > 0.2f)
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical");
+
+
+        if (true)
         {
-            if (ani.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            if (!hasSetBallDir)
             {
-                setAnimation("Run");
+                if (rigbod.velocity.x > 0)
+                {
+                    transform.localScale = new Vector3(1, 1, 1) * referenceScale;
+                }
+                else if (rigbod.velocity.x < 0)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1) * referenceScale;
+                }
+                hasSetBallDir = true;
             }
+
+            setAnimation("Roll", false);
+            ani.speed = rigbod.velocity.magnitude / 4;
         }
         else
         {
-            setAnimation("Idle");
+            if (playerInput.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1) * referenceScale;
+            }
+            else if (playerInput.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1) * referenceScale;
+            }
+            hasSetBallDir = false;
+
+            if (playerInput.magnitude > 0)
+            {
+                if (ani.GetCurrentAnimatorStateInfo(0).IsName("Idle") || ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.1f)
+                {
+                    setAnimation("Run", true);
+                }
+            }
+            else
+            {
+                if (!ani.GetCurrentAnimatorStateInfo(0).IsName("Run") || ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.1f)
+                {
+                    setAnimation("Idle", false);
+                }
+            }
         }
     }
 }
