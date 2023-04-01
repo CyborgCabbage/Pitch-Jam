5398b3a01ba4954f3ddca0c5ab40090f284df2e6
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-04-01 20:09:21 +0100

    Zombie animation and circle light motion

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float time = 1;
+    [SerializeField] public Animator animator;
+    public float time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Walking", transform.GetChild(0).GetComponent<AIDetector>().Target != null);
+        
         if (transform.GetChild(0).GetComponent<AIDetector>().PlayerDetected)
         {
             StartCoroutine(MoveCoroutine());
         }
+      
         if (transform.GetChild(1).GetComponent<AiMeleeDetector>().PlayerDetected)
         {
-
             Vector2 force = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             PlayerMovement player = transform.GetChild(0).GetComponent<AIDetector>().Target.GetComponent<PlayerMovement>();
             if(time <= 0)
@@ -36,6 +39,7 @@ public class Enemy : MonoBehaviour
             StopAllCoroutines();
         }
         time -= Time.deltaTime;
+        animator.SetFloat("Kick", time);
     }
 
     IEnumerator MoveCoroutine()
@@ -43,9 +47,11 @@ public class Enemy : MonoBehaviour
         Vector2 target = transform.GetChild(0).GetComponent<AIDetector>().Target.transform.position;
         while (Vector2.Distance(transform.position, target) > 0)
         {
+            animator.GetComponent<SpriteRenderer>().flipX = target.x > transform.position.x;
             transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime);
             yield return new WaitForSeconds(0.1f);
         }
+        
     }
 
 
