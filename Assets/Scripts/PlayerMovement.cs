5ee254d70d0a1e670a2317f0bd5a8842652e001b
Julian Banks <julianb7048@gmail.com>
2023-04-01 12:19:00 +0100

    Finished basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Camera cam;
 
+    [SerializeField] PhysicsMaterial2D bounceMaterial;
+
     [SerializeField] float moveSpeed;
     [SerializeField] float ballSpeed;
+    [SerializeField] float minBallSpeedForSwitch;
 
     Vector2 ballTrajectory;
     Vector2 playerInput;
@@ -28,22 +31,36 @@ public class PlayerMovement : MonoBehaviour
         playerInput.y = Input.GetAxisRaw("Vertical");
 
         //Check for roll button
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")) //LMB
         {
+            //If they are running - turn into ball
             if (!isBall)
             {
+                //Make player bounce off bounds
+                rb.sharedMaterial = bounceMaterial;
+
+                //Get player direction to mouse direction for trajectory
                 Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 diff = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
                 diff.Normalize();
                 ballTrajectory = diff;
+
+                //Shoot player
                 rb.velocity = new Vector2(ballTrajectory.x * ballSpeed, ballTrajectory.y * ballSpeed);
 
                 isBall = true;
             }
             else
             {
+                //Manual ball cancel, start running and dont bounce
+                rb.sharedMaterial = null;
                 isBall = false;
             }
+        }
+
+        if (isBall && rb.velocity.magnitude < minBallSpeedForSwitch)
+        {
+            isBall = false;
         }
     }
 
@@ -54,10 +71,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(playerInput.x * moveSpeed * Time.fixedDeltaTime, playerInput.y * moveSpeed * Time.fixedDeltaTime);
         }
-        else
-        {
-           // rb.velocity = new Vector2(ballTrajectory.x * ballSpeed * Time.fixedDeltaTime, ballTrajectory.y * ballSpeed * Time.fixedDeltaTime);
-        }
+      
     }
 
     public Vector3 GetVelocity()
