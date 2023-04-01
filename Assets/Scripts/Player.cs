56b0112f3a05367f6347770feb22cca6c1114232
Julian Banks <julianb7048@gmail.com>
2023-04-01 14:17:27 +0100

    Made movement great again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,4 +6,20 @@ public class Player : MonoBehaviour
 {
     int score;
     int health;
+    [SerializeField] Goal[] goals;
+    int targetGoal;
+
+    public void IncrementGoals()
+    {
+        score++;
+        for (int i = 0; i < goals.Length; i++)
+        {
+           goals[i].SwapActiveGoals();
+        }
+    }
+
+    public void DecreaseHealth(float decrease)
+    {
+
+    }
 }
