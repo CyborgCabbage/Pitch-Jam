367eefec21fc7e0def3041c670a2c3af018a4f9c
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-04-02 18:24:21 +0100

    Fix enemy issue

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -34,12 +34,15 @@ public class Enemy : MonoBehaviour
         if (transform.GetChild(1).GetComponent<AiMeleeDetector>().PlayerDetected)
         {
             Vector2 force = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            PlayerMovement player = transform.GetChild(0).GetComponent<AIDetector>().Target.GetComponent<PlayerMovement>();
-            if(time <= 0)
-            {
-                audio.PlayOneShot(sfx[0]);
-                player.Roll(force, 50);
-                time = 1;
+            GameObject target = transform.GetChild(0).GetComponent<AIDetector>().Target;
+            if (target != null) {
+                PlayerMovement player = target.GetComponent<PlayerMovement>();
+                if (time <= 0)
+                {
+                    audio.PlayOneShot(sfx[0]);
+                    player.Roll(force, 50);
+                    time = 1;
+                }
             }
         }
         else
