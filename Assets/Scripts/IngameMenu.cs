f4efee29bf811cffe2b263f7930b679fb1a09506
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-04-02 15:47:50 +0100

    Score saving

--- Assets/Scripts/IngameMenu.cs
+++ Assets/Scripts/IngameMenu.cs
@@ -1,21 +1,96 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
+using Newtonsoft.Json;
+[System.Serializable]
+public class SaveData {
+    public Dictionary<string, int> scores = new();
+}
+
 public class IngameMenu : MonoBehaviour
 {
     [SerializeReference] public GameObject pauseMenu;
     [SerializeReference] public GameObject deathMenu;
     [SerializeField] public GameObject scoreText;
     [SerializeReference] public Player player;
+    [SerializeReference] public TMPro.TextMeshProUGUI myScore;
+    [SerializeReference] public TMPro.TextMeshProUGUI otherScores;
+    [SerializeReference] public TMPro.TMP_InputField textBox;
+    
     public static bool isPaused = false;
     bool reloading = false;
-    // Start is called before the first frame update
+    SaveData saveData;
+    public string playerName;
+
+    string LoadFile(string name) {
+        string fullPath = Path.Combine(Application.persistentDataPath, name);
+        try
+        {
+            return File.ReadAllText(fullPath);
+        }
+        catch (Exception)
+        {
+            return "{}";
+        }
+    }
+    void SaveFile(string name, string contents) {
+        var fullPath = Path.Combine(Application.persistentDataPath, name);
+
+        try
+        {
+            File.WriteAllText(fullPath, contents);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to write to {fullPath} with exception {e}");
+        }
+    }
     void Start()
     {
         pauseMenu.SetActive(false);
         deathMenu.transform.GetChild(0).gameObject.SetActive(false);
+        try
+        {
+            saveData = JsonConvert.DeserializeObject<SaveData>(LoadFile("SaveData.dat"));
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to read with exception {e}");
+            saveData = new SaveData();
+        }
+    }
+
+    public void SetName(string s) {
+        playerName = s;
+    }
+
+    public void SaveHighScore() {
+        if (playerName == null) return;
+        if (playerName.Length == 0) return;
+        textBox.interactable = false;
+        if (saveData.scores.ContainsKey(playerName))
+        {
+            int oldScore = saveData.scores[playerName];
+            if (Player.score <= oldScore) return;
+        }
+        saveData.scores[playerName] = Player.score;
+        SaveFile("SaveData.dat", JsonConvert.SerializeObject(saveData));
+        RefreshLeaderBoard();
+    }
+
+    void RefreshLeaderBoard() {
+        var list = saveData.scores.OrderByDescending(pair => pair.Value).ToList();
+        string thing = "";
+        for (int i = 0; i < Mathf.Min(7, list.Count()); i++) {
+            thing += list[i].Key + " " + list[i].Value + "\n";
+        }
+        otherScores.text = thing;
     }
 
     // Update is called once per frame
@@ -44,6 +119,8 @@ public class IngameMenu : MonoBehaviour
         if (!player.isAlive)
         {
             deathMenu.transform.GetChild(0).gameObject.SetActive(true);
+            RefreshLeaderBoard();
+            myScore.text = "YOU SCORED " + Player.score;
             TogglePaused();
         }
     }
