619a76743bd72c578069c77c25017c3417e95957
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-04-02 12:32:26 +0100

    Change around ingame menu

--- Assets/Scripts/IngameMenu.cs
+++ Assets/Scripts/IngameMenu.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class PauseMenu : MonoBehaviour
+using UnityEngine.UI;
+public class IngameMenu : MonoBehaviour
 {
     [SerializeReference] public GameObject pauseMenu;
+    [SerializeReference] public GameObject deathMenu;
     [SerializeField] public GameObject scoreText;
+    [SerializeReference] public Player player;
     public static bool isPaused = false;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,18 @@ public class PauseMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePaused();
+        }
+        if (!isPaused) {
+            Image image = deathMenu.GetComponent<Image>();
+            float a = image.color.a;
+            if (player.lightCount == 0)
+            {
+                a += Time.deltaTime * 0.25f;
+            }
+            else {
+                a -= Time.deltaTime * 0.25f;
+            }
+            image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Clamp01(a));
         }
     }
     public void TogglePaused() {
